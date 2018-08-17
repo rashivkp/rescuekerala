15cfe3ffabe1d8f7b31bad8059b6fbd4be249bf5
Mohamed Rashid <rashivkp@gmail.com>
2018-08-17 10:28:31 +0530

    form for user creation added

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('register/', views.proces_log, name='home'),
+    path('add_volunteer/', views.create_volunteer, name='home'),
+    path('approve_volunteer/', views.approve_volunteer, name='home'),
     path('view/<int:pk>/', views.view_request, name='view_request'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,6 +6,8 @@ from .models import Request, RequestLog
 import django_filters
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 import json
+from django.contrib.auth.models import User, Group
+from django import forms
 
 def proces_log(request):
     requestee_phone = request.GET.get('From')
@@ -14,6 +16,37 @@ def proces_log(request):
         obj, created = Request.objects.get_or_create(requestee_phone=requestee_phone,
                 status='new')
         RequestLog.objects.create(request=obj, details=json.dumps(request.GET))
+
+    return HttpResponse('success')
+
+def create_volunteer(request):
+    if request.method == 'GET':
+        return render(request, 'mainapp/volunteer_form.html')
+
+    volunteer_phone = request.POST.get('mobile')
+    password = request.POST.get('password')
+
+    if volunteer_phone:
+        if User.objects.filter(username=volunteer_phone).count():
+            return HttpResponse('already exists')
+        user = User.objects.create_user(volunteer_phone, '', password)
+        user.is_staff = True
+        user.is_active = False
+        user.save()
+        group, created = Group.objects.get_or_create(name='Volunteer')
+        group.user_set.add(user)
+        return render(request, 'volunteer_created.html', {'user':user})
+
+    return HttpResponse('')
+
+def approve_volunteer(request):
+    volunteer_phone = request.GET.get('From')
+
+    if volunteer_phone:
+        if User.objects.filter(username=volunteer_phone).count():
+            user.is_active = False
+            return HttpResponse('appproved')
+        return HttpResponse('not found')
 
     return HttpResponse('')
 
